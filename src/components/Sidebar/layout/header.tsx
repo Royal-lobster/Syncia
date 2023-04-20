db28bcb9c477fee68b633085f3774d6962644f65
Srujan Gurram <srujangs8@gmail.com>
2023-04-21 03:28:32 +0530

    changed header

--- src/components/Sidebar/layout/header.tsx
+++ src/components/Sidebar/layout/header.tsx
@@ -1,3 +1,4 @@
+import { BsRobot } from 'react-icons/bs'
 import { HiOutlineCog, HiX } from 'react-icons/hi'
 
 const Header = () => {
@@ -9,7 +10,10 @@ const Header = () => {
 
   return (
     <div className="cdx-flex cdx-justify-between cdx-p-3.5 cdx-border-b dark:cdx-border-neutral-700/50 cdx-border-neutral-200">
-      <h1 className="cdx-text-2xl cdx-m-0 cdx-p-0">Syncia</h1>
+      <h1 className="cdx-text-2xl cdx-flex cdx-items-center cdx-gap-2 cdx-m-0 cdx-p-0">
+        <BsRobot className='cdx-text-blue-400' />
+        Syncia
+      </h1>
       <div className='cdx-flex cdx-text-neutral-500 cdx-gap-2 cdx-items-center'>
         <a
           target='_blank'
