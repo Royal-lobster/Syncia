1cba0d9387327dcca4208d961cd53a2ccf8a0809
Srujan Gurram <srujangs8@gmail.com>
2023-04-21 12:46:39 +0530

    Refactor validApiKey.ts to use single quotes consistently and simplify catch block

--- src/utils/validApiKey.ts
+++ src/utils/validApiKey.ts
@@ -1,17 +1,17 @@
-import axios from "axios";
+import axios from 'axios'
 
-const VERIFY_API_KEY_URL = "https://api.openai.com/v1/models";
+const VERIFY_API_KEY_URL = 'https://api.openai.com/v1/models'
 
 export const validateApiKey = async (apiKey: string): Promise<boolean> => {
   try {
     await axios.get(VERIFY_API_KEY_URL, {
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${apiKey}`,
       },
-    });
-    return true;
-  } catch (error) {
-    return false;
+    })
+    return true
+  } catch {
+    return false
   }
-};
+}
