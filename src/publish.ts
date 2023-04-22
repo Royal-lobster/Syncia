6bc678187a72b4f0f29cc7649717e3a4868a2ba9
Srujan Gurram <srujangs8@gmail.com>
2023-04-22 17:51:22 +0530

    Add environment variables for Chrome credentials in publish.ts

--- src/publish.ts
+++ src/publish.ts
@@ -4,11 +4,11 @@ publishExtension({
   dryRun: true,
   chrome: {
     zip: 'artifacts/chrome.zip',
-    extensionId: '<cws-extension-id>',
-    clientId: '<gcp-client-id>',
-    clientSecret: '<gcp-client-secret>',
-    refreshToken: '<gcp-refresh-token>',
-    publishTarget: '<default|trustedTesters>',
+    extensionId: '',
+    clientId: process.env.CHROME_CLIENT_ID as string,
+    clientSecret: process.env.CHROME_CLIENT_SECRET as string,
+    refreshToken: process.env.CHROME_REFRESH_TOKEN as string,
+    publishTarget: 'default',
     skipSubmitReview: false,
   },
 })
