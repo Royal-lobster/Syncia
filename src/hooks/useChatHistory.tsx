c067c380e6d792fb467affef97a50060b3e85579
Adebesin Cell <adebesintolulope80@gmail.com>
2023-09-29 05:11:28 +0100

    [ChatHistory] Fix import statements and remove unnecessary semicolons

--- src/hooks/useChatHistory.tsx
+++ src/hooks/useChatHistory.tsx
@@ -1,21 +1,21 @@
-import { useMemo } from 'react';
-import { getUUID } from '../lib/getUUID';
-import { useStorage } from './useStorage';
+import { useMemo } from 'react'
+import { getUUID } from '../lib/getUUID'
+import { useStorage } from './useStorage'
 
 interface ChatHistory {
-  id: string;
-  name: string;
-  createdAt: string;
-  updatedAt: string;
+  id: string
+  name: string
+  createdAt: string
+  updatedAt: string
 }
 
 export const useChatHistory = () => {
-  const [history, setHistory] = useStorage<ChatHistory[]>('HISTORY', []);
-  const initialChatId = useMemo(getUUID, []);
+  const [history, setHistory] = useStorage<ChatHistory[]>('HISTORY', [])
+  const initialChatId = useMemo(getUUID, [])
   const [currentChatId, setCurrentChatId] = useStorage<string>(
     'CURRENT_CHAT_ID',
-    initialChatId
-  );
+    initialChatId,
+  )
 
   const createChatHistory = (name: string, newId = getUUID()) => {
     setHistory((prev) => [
@@ -24,42 +24,42 @@ export const useChatHistory = () => {
         id: newId,
         name,
         createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString()
-      }
-    ]);
-    setCurrentChatId(newId);
+        updatedAt: new Date().toISOString(),
+      },
+    ])
+    setCurrentChatId(newId)
 
-    return newId;
-  };
+    return newId
+  }
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const tab = tabs[0];
+    const tab = tabs[0]
     if (tab) {
-      console.log(tab);
+      console.log(tab)
     }
-  });
+  })
 
   const createChatHistoryFromActiveTab = () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const tab = tabs[0] as any;
+      const tab = tabs[0] as any
       if (tab) {
-        const data = JSON.parse(tab.vivExtData);
-        const url = data.urlForThumbnail;
-        const title = new URL(url).hostname;
-        createChatHistory(title);
+        const data = JSON.parse(tab.vivExtData)
+        const url = data.urlForThumbnail
+        const title = new URL(url).hostname
+        createChatHistory(title)
       }
-    });
-  };
+    })
+  }
 
   if (currentChatId === initialChatId) {
-    createChatHistoryFromActiveTab();
+    createChatHistoryFromActiveTab()
   }
 
   const deleteChatHistory = (id: string) => {
-    setHistory((prev) => prev.filter((h) => h.id !== id));
-  };
+    setHistory((prev) => prev.filter((h) => h.id !== id))
+  }
   const getChatHistory = (id: string) => {
-    return history.find((h) => h.id === id);
-  };
+    return history.find((h) => h.id === id)
+  }
 
   return {
     currentChatId,
@@ -68,6 +68,6 @@ export const useChatHistory = () => {
     deleteChatHistory,
     getChatHistory,
     createChatHistoryFromActiveTab,
-    history
-  };
-};
+    history,
+  }
+}
