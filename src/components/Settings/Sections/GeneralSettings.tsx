059cfe4ecc8422b48eb1d32bffda21f6b46b9f64
Srujan Gurram <srujangs8@gmail.com>
2023-11-25 20:28:29 +0530

    added webpage context to settings

--- src/components/Settings/Sections/GeneralSettings.tsx
+++ src/components/Settings/Sections/GeneralSettings.tsx
@@ -3,6 +3,7 @@ import SectionHeading from '../Elements/SectionHeading'
 import FieldWrapper from '../Elements/FieldWrapper'
 import { useSettings } from '../../../hooks/useSettings'
 import { ThemeOptions } from '../../../config/settings'
+import * as Switch from '@radix-ui/react-switch'
 
 const GeneralSettings = () => {
   const [settings, setSettings] = useSettings()
@@ -39,6 +40,27 @@ const GeneralSettings = () => {
             </option>
           ))}
         </select>
+      </FieldWrapper>
+      <FieldWrapper
+        title="Webpage Context"
+        description="Enable Syncia to answer questions based on the current webpage content"
+        row
+      >
+        <Switch.Root
+          checked={generalSettings.webpageContext}
+          onCheckedChange={(value) =>
+            setSettings({
+              ...settings,
+              general: {
+                ...generalSettings,
+                webpageContext: value,
+              },
+            })
+          }
+          className="cdx-w-[42px] cdx-h-[25px] cdx-bg-neutral-500 cdx-rounded-full cdx-relative data-[state=checked]:cdx-bg-blue-500 cdx-outline-none cdx-cursor-default"
+        >
+          <Switch.Thumb className="cdx-block cdx-w-[21px] cdx-h-[21px] cdx-bg-white cdx-rounded-full cdx-transition-transform cdx-duration-100 cdx-translate-x-0.5 cdx-will-change-transform data-[state=checked]:cdx-translate-x-[19px]" />
+        </Switch.Root>
       </FieldWrapper>
     </div>
   )
