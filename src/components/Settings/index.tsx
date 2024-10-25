f9e8022cc3cd335cfb0bb89f52978872088985e8
Srujan Gurram <srujangs8@gmail.com>
2024-10-25 21:12:52 +0530

    Update biome format and biome check commands. Fix styling in SectionHeading component and Settings components. Fix formatting in sidebar index.css

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -9,9 +9,9 @@ import PromptSettings from './Sections/PromptSettings'
 const Settings = () => {
   useThemeSync()
   return (
-    <div className='cdx-container cdx-mx-auto cdx-p-5 cdx-pt-[10vh]'>
+    <div className="cdx-container cdx-mx-auto cdx-p-5 cdx-pt-[10vh]">
       <Header />
-      <div className='cdx-flex cdx-flex-col xl:cdx-flex-row cdx-justify-between cdx-gap-10 cdx-flex-wrap cdx-mt-10'>
+      <div className="cdx-flex cdx-flex-col xl:cdx-flex-row cdx-justify-between cdx-gap-10 cdx-flex-wrap cdx-mt-10">
         <div className="cdx-flex cdx-flex-1 cdx-flex-col cdx-gap-4 cdx-self-end cdx-sticky cdx-bottom-0">
           <GeneralSettings />
           <QuickMenuSettings />
