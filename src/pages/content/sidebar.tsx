751273f844900183adb9085b4cae01208adfe27c
Adebesin Cell <adebesintolulope80@gmail.com>
2024-12-28 09:47:12 +0100

    chore: fixes formatting with biome

--- src/pages/content/sidebar.tsx
+++ src/pages/content/sidebar.tsx
@@ -1,23 +1,23 @@
-import type { Settings } from '../../config/settings';
-import { getScreenshotImage } from '../../lib/getScreenshotImage';
-import { contentScriptLog } from '../../logs';
+import type { Settings } from '../../config/settings'
+import { getScreenshotImage } from '../../lib/getScreenshotImage'
+import { contentScriptLog } from '../../logs'
 
-contentScriptLog('Sidebar');
+contentScriptLog('Sidebar')
 
-const iframe = document.createElement('iframe');
-iframe.style.background = '#0000002a';
-iframe.style.height = '100%';
-iframe.style.width = '0px';
-iframe.style.position = 'fixed';
-iframe.style.top = '0px';
-iframe.style.right = '0px';
-iframe.style.zIndex = '9000000000000000000';
-iframe.style.border = '0px';
-iframe.style.colorScheme = 'auto';
-iframe.src = chrome.runtime.getURL('/src/pages/sidebar/index.html');
-iframe.id = 'syncia_sidebar';
+const iframe = document.createElement('iframe')
+iframe.style.background = '#0000002a'
+iframe.style.height = '100%'
+iframe.style.width = '0px'
+iframe.style.position = 'fixed'
+iframe.style.top = '0px'
+iframe.style.right = '0px'
+iframe.style.zIndex = '9000000000000000000'
+iframe.style.border = '0px'
+iframe.style.colorScheme = 'auto'
+iframe.src = chrome.runtime.getURL('/src/pages/sidebar/index.html')
+iframe.id = 'syncia_sidebar'
 
-document.body.appendChild(iframe);
+document.body.appendChild(iframe)
 
 /**
  * BG SCRIPT <-> CONTENT SCRIPT
@@ -28,12 +28,12 @@ document.body.appendChild(iframe);
 chrome.runtime.onMessage.addListener((msg) => {
   if (msg.action === 'open-sidebar') {
     if (iframe.style.width === '0px') {
-      iframe.style.width = '400px';
+      iframe.style.width = '400px'
     } else {
-      iframe.style.width = '0px';
+      iframe.style.width = '0px'
     }
   }
-});
+})
 
 /**
  * SIDEBAR <-> CONTENT SCRIPT
@@ -42,37 +42,37 @@ chrome.runtime.onMessage.addListener((msg) => {
  * The page content is sent back to the sidebar by posting a message with the action 'get-page-content'.
  */
 window.addEventListener('message', async (event) => {
-  const { action, _payload } = event.data as { action: string; _payload: any };
+  const { action, _payload } = event.data as { action: string; _payload: any }
 
   // ACTION: get-page-content ==============================
   if (action === 'get-page-content') {
-    const pageContent = document.body.innerText;
+    const pageContent = document.body.innerText
     iframe.contentWindow?.postMessage(
       {
         action: 'get-page-content',
-        payload: pageContent
+        payload: pageContent,
       },
-      '*'
-    );
+      '*',
+    )
   }
 
   // ACTION: copy-to-clipboard =============================
   if (action === 'copy-to-clipboard') {
-    const { content } = _payload as { content: string };
-    navigator.clipboard.writeText(content);
+    const { content } = _payload as { content: string }
+    navigator.clipboard.writeText(content)
   }
 
   // ACTION: get-screenshot-image ===========================
   if (action === 'get-screenshot-image') {
-    iframe.style.width = '0px';
-    const image = await getScreenshotImage();
-    iframe.style.width = '400px';
+    iframe.style.width = '0px'
+    const image = await getScreenshotImage()
+    iframe.style.width = '400px'
     iframe.contentWindow?.postMessage(
       {
         action: 'get-screenshot-image',
-        payload: image
+        payload: image,
       },
-      '*'
-    );
+      '*',
+    )
   }
-});
+})
